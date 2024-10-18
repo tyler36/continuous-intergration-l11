5b5b9adf8c2d897923c8c750f43ee53dfbe67b4f
tyler36 <7234392+tyler36@users.noreply.github.com>
2024-10-18 17:52:59 +0900

    BAD: move bracket

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,8 +37,7 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
-    protected function casts(): array
-    {
+    protected function casts(): array {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
