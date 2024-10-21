0c01a222eb36728b9c6bbed09b92698ca75833e1
gh-actions <gh@example.com>
2024-10-21 00:18:39 +0000

    Laravel pint

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,7 +37,8 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
-    protected function casts(): array {
+    protected function casts(): array
+    {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
