85979ba1f6445b3d8d64904e69a207e533037e86
tyler36 <7234392+tyler36@users.noreply.github.com>
2024-10-21 10:03:18 +0900

    run tests without Vite

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,13 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    /**
+     * Test setup.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutVite();
+    }
 }
